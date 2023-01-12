640aa6ebd37ee4e2e7ed5181b3e50ea2da949493
Robert <rj.robertjas@gmail.com>
2023-01-12 21:01:05 +0100

    Funkcja dodawania slow

---
+++ app/Http/Controllers/DictionaryController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dictionary;
+use App\Http\Requests\StoreDictionaryRequest;
+use App\Http\Requests\UpdateDictionaryRequest;
+
+class DictionaryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreDictionaryRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreDictionaryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Dictionary  $dictionary
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Dictionary $dictionary)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Dictionary  $dictionary
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Dictionary $dictionary)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateDictionaryRequest  $request
+     * @param  \App\Models\Dictionary  $dictionary
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateDictionaryRequest $request, Dictionary $dictionary)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Dictionary  $dictionary
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Dictionary $dictionary)
+    {
+        //
+    }
+}
